b181855e54bd0a4ef7531f094e4d92ffba83cc16
Diego Fernandez <dfernandez4@usf.edu>
2023-01-16 18:43:14 -0500

    Add project files.

---
+++ HelloWorld1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Deliverable_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string stg = "Hello, World! (2)";
+            Console.WriteLine(stg);
+            Console.WriteLine("Enter username");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Username is: " + userName);
+            Console.WriteLine("Enter user's age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("User's age is: " + age);
+        }
+    }
+}
